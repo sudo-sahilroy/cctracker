9ed559c3029d9bd1006f500ee5897e4f8d41085a
Sharad Saha <sahasharad29@gmail.com>
2022-11-09 21:33:51 +0530

    added video upload feature

--- website/views.py
+++ website/views.py
@@ -1,8 +1,19 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,redirect
+from werkzeug.utils import secure_filename
+import os
 
 views = Blueprint('views',__name__)
 
 @views.route('/')
 def home():
     return render_template("index.html")
+
+
+@views.route('/upload',methods=['POST'])
+def upload():
+    file = request.files['file']
+
+    file.save(f'website/static/uploads/{secure_filename(file.filename)}')
+
+    return redirect('/')
 
