9fee648a23d4cc822a9bec4c08f06606cb1d2de3
Sahil <sahilroy2001.sr@gmail.com>
2022-11-10 12:43:40 +0530

    made changes to views

--- website/views.py
+++ website/views.py
@@ -1,8 +1,12 @@
 from flask import Blueprint,render_template,request,redirect
 from werkzeug.utils import secure_filename
 import os
+# import tensorflow.compat.v2 as tf
+# import tensorflow_hub as hub
 
 views = Blueprint('views',__name__)
+
+hub_model = None
 
 @views.route('/')
 def home():
@@ -11,9 +15,11 @@ def home():
 
 @views.route('/upload',methods=['POST'])
 def upload():
+    global hub_model
     file = request.files['file']
 
-    file.save(f'website/static/uploads/{secure_filename(file.filename)}')
-
+    # file.save(f'website/static/uploads/{secure_filename(file.filename)}')
+    # hub_handle = 'https://tfhub.dev/deepmind/mil-nce/s3d/1'
+    # hub_model = hub.load(hub_handle)
     return redirect('/')
 
